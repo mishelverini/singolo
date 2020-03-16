ecd71a4e49e32fba12368991504e69f0ef87e1b6
Mikhail Vazhynski <mishelverini@yahoo.com>
2020-03-16 14:34:12 +0300

    feat: add slider

---
+++ script.js
@@ -0,0 +1,59 @@
+let i = 0;
+let s1 = true;
+let s2 = true;
+
+let slideRight = function () {
+
+    document.querySelectorAll('.slider_item').forEach(function (element) {
+
+        element.style.left = `${i}px`;
+
+        if (element.style.left === '-940px') {
+            s1 = false;
+            clearInterval(slideRight);
+            return;
+        }
+        if (s1) {
+            i -= 10;
+        }
+        console.log(i);
+    });
+};
+
+let slideLeft = function () {
+
+    document.querySelectorAll('.slider_item').forEach(function (element) {
+
+        element.style.left = `${i}px`;
+
+        if (element.style.left === '0px') {
+            s2 = false;
+            clearInterval(slideLeft);
+            return;
+        }
+        if (s2) {
+            i += 10;
+        }
+
+        console.log(i);
+    });
+};
+
+function sRight() {
+    if (i == -940) {
+        document.querySelectorAll('.slider_item').forEach(element => element.style.left = '0px');
+        i = 0;
+    } else {
+        setInterval(slideRight, 1);
+        s1 = true;
+    }
+}
+function sLeft() {
+    if (i == 0) {
+        document.querySelectorAll('.slider_item').forEach(element => element.style.left = '-940px');
+        i = -940;
+    } else {
+        setInterval(slideLeft, 1);
+        s2 = true;
+    }
+}
